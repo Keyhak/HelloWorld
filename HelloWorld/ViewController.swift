b14cf45911cbc08bb33d6167da173f604a02ba5f
Takahiro <jzghx65z@icloud.com>
2023-03-21 12:24:32 +0900

    ha

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dogButton(_ sender: Any) {
+        label.text = "ワンワン"
+    }
 }
